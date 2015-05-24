e3a0c500d402cbdf8903a9f149b4920d65a2d94c
Alin Eugen Deac <aedart@gmail.com>
2015-05-24 10:23:50 +0200

    Internal Collection trait and unit test completed

---
+++ src/Traits/InternalCollectionTrait.php
@@ -0,0 +1,61 @@
+<?php namespace Aedart\Util\Traits;
+
+use Illuminate\Support\Collection;
+
+/**
+ * Trait Internal Collection
+ *
+ * Offers components a getter and setter for an internal / protected
+ * Laravel collection
+ *
+ * @see \Illuminate\Support\Collection
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Util\Traits
+ */
+trait InternalCollectionTrait {
+
+    /**
+     * This collection's internal collection.
+     * It is being used to store the actual values
+     *
+     * @var \Illuminate\Support\Collection|null
+     */
+    protected $internalCollection = null;
+
+    /**
+     * Set the internal collection, which contain the actual
+     * values of this collection
+     *
+     * @param \Illuminate\Support\Collection $internalCollection
+     */
+    protected function setInternalCollection(Collection $internalCollection){
+        $this->internalCollection = $internalCollection;
+    }
+
+    /**
+     * Get the internal collection, which contain the actual
+     * values of this collection
+     *
+     * If no internal collection has been set, this method will
+     * set and return a default empty collection
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    protected function getInternalCollection(){
+        if(is_null($this->internalCollection)){
+            $this->setInternalCollection($this->getDefaultInternalCollection());
+        }
+        return $this->internalCollection;
+    }
+
+    /**
+     * Returns a default empty collection
+     *
+     * @return Collection A default empty collection
+     */
+    protected function getDefaultInternalCollection(){
+        return new Collection();
+    }
+
+}
---
+++ tests/unit/collection/InternalCollectionTest.php
@@ -0,0 +1,86 @@
+<?php
+
+use Aedart\Util\Traits\InternalCollectionTrait;
+use Illuminate\Support\Collection;
+
+/**
+ * Class InternalCollectionTest
+ *
+ * @coversDefaultClass Aedart\Util\Traits\InternalCollectionTrait
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ */
+class InternalCollectionTest extends \Codeception\TestCase\Test
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    /*********************************************************************************
+     * Helpers
+     ********************************************************************************/
+
+    /**
+     * Get a dummy internal collection
+     *
+     * @return DummyInternalCollection
+     */
+    protected function getDummyInternalCollection(){
+        return new DummyInternalCollection();
+    }
+
+    /*********************************************************************************
+     * Actual tests
+     ********************************************************************************/
+
+    /**
+     * @test
+     * @covers ::setInternalCollection
+     * @covers ::getInternalCollection
+     * @covers ::getDefaultInternalCollection
+     */
+    public function getDefaultInternalCollection(){
+        $collection = $this->getDummyInternalCollection()->getInternalCollection();
+        $this->assertNotNull($collection, 'Expected a collection - something is off!');
+        $this->assertInstanceOf('Illuminate\Support\Collection', $collection);
+    }
+}
+
+/**
+ * Class Dummy Internal Collection
+ *
+ * For testing only - exposes the protected methods
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ */
+class DummyInternalCollection {
+
+    use InternalCollectionTrait {
+        setInternalCollection as setInternalCollectionTrait;
+        getInternalCollection as getInternalCollectionTrait;
+        getDefaultInternalCollection as getDefaultInternalCollectionTrait;
+    }
+
+    public function setInternalCollection(Collection $internalCollection) {
+        $this->setInternalCollectionTrait($internalCollection);
+    }
+
+    public function getInternalCollection() {
+        return $this->getInternalCollectionTrait();
+    }
+
+    public function getDefaultInternalCollection() {
+        return $this->getDefaultInternalCollectionTrait();
+    }
+
+
+}
