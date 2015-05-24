82f6ee278384c610ec8e51ae2b47766b2f515cdc
Alin Eugen Deac <aedart@gmail.com>
2015-05-24 11:00:11 +0200

    PartialCollectionTrait and unit test completed

---
+++ src/Traits/PartialCollectionTrait.php
@@ -0,0 +1,100 @@
+<?php  namespace Aedart\Util\Traits;
+
+use Aedart\Util\Interfaces\IPartialCollection;
+use Aedart\Util\Traits\InternalCollectionTrait;
+use Illuminate\Support\Collection;
+use Traversable;
+
+/**
+ * Trait Partial Collection
+ *
+ * This trait offers some predefined implementation of a "partial" collection, by
+ * making use of Laravel's Collection class. Thus, this trait acts as a wrapper
+ * or decorator for such a collection.
+ *
+ * However, not all methods are offered by this trait. The reason for this is to
+ * allow you to decide how your concrete collection should behave.
+ *
+ * @see \Aedart\Util\Interfaces\IPartialCollection
+ * @see \Illuminate\Support\Collection
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Util\Traits
+ */
+trait PartialCollectionTrait {
+
+    use InternalCollectionTrait;
+
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * Count elements of an object
+     * @link http://php.net/manual/en/countable.count.php
+     * @return int The custom count as an integer.
+     * </p>
+     * <p>
+     * The return value is cast to an integer.
+     */
+    public function count() {
+        return $this->getInternalCollection()->count();
+    }
+
+    /**
+     * Check if this collection is empty or not
+     *
+     * @return bool True if empty, false if not
+     */
+    public function isEmpty() {
+        return $this->getInternalCollection()->isEmpty();
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Retrieve an external iterator
+     * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
+     * @return Traversable An instance of an object implementing <b>Iterator</b> or
+     * <b>Traversable</b>
+     */
+    public function getIterator() {
+        return $this->getInternalCollection()->getIterator();
+    }
+
+    /**
+     * Get the instance as an array.
+     *
+     * @return array
+     */
+    public function toArray() {
+        return $this->getInternalCollection()->toArray();
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.4.0)<br/>
+     * Specify data which should be serialized to JSON
+     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     */
+    public function jsonSerialize() {
+        return $this->toArray();
+    }
+
+    /**
+     * Convert the object to its JSON representation.
+     *
+     * @param  int $options
+     *
+     * @return string
+     */
+    public function toJson($options = 0) {
+        return json_encode($this->jsonSerialize(), $options);
+    }
+
+    /**
+     * Get the string representation of this collection
+     *
+     * @return string Representation of this collection
+     */
+    public function __toString() {
+        return $this->toJson();
+    }
+}
---
+++ tests/unit/collection/PartialCollectionTraitTest.php
@@ -0,0 +1,144 @@
+<?php
+
+use Aedart\Util\Traits\PartialCollectionTrait;
+
+/**
+ * Class PartialCollectionTraitTest
+ *
+ * @coversDefaultClass Aedart\Util\Traits\PartialCollectionTrait
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ */
+class PartialCollectionTraitTest extends CollectionTestCase
+{
+
+    /*********************************************************************************
+     * Helpers
+     ********************************************************************************/
+
+    /**
+     * Get a dummy partial collection implementation
+     *
+     * @return DummyPartialCollection
+     */
+    protected function getDummyCollection(){
+        return new DummyPartialCollection();
+    }
+
+    /*********************************************************************************
+     * Actual tests
+     ********************************************************************************/
+
+    /**
+     * @test
+     * @covers ::count
+     */
+    public function countItems(){
+        $collection = $this->getDummyCollection();
+
+        $list = $this->getListOfKeyValuePairs();
+
+        $collection->addFrom($list);
+        $collection->put($this->faker->word, $this->faker->sentence());
+
+        $this->assertSame(count($list) + 1, $collection->count());
+    }
+
+    /**
+     * @test
+     * @covers ::isEmpty
+     */
+    public function collectionIsEmpty(){
+        $collection = $this->getDummyCollection();
+        $this->assertTrue($collection->isEmpty());
+    }
+
+    /**
+     * @test
+     * @covers ::isEmpty
+     */
+    public function collectionIsNotEmpty(){
+        $collection = $this->getDummyCollection();
+
+        $collection->put($this->faker->word, $this->faker->sentence());
+
+        $this->assertFalse($collection->isEmpty());
+    }
+
+    /**
+     * @test
+     * @covers ::getIterator
+     */
+    public function getCollectionIterator(){
+        $collection = $this->getDummyCollection();
+        $this->assertInstanceOf('Traversable', $collection->getIterator());
+    }
+
+    /**
+     * @test
+     * @covers ::toArray
+     */
+    public function exportToArray(){
+        $collection = $this->getDummyCollection();
+
+        $list = $this->getListOfKeyValuePairs();
+        $collection->addFrom($list);
+
+        $this->assertSame($list, $collection->toArray());
+    }
+
+    /**
+     * @test
+     * @covers ::jsonSerialize
+     */
+    public function getTheJsonSerializableData(){
+        $collection = $this->getDummyCollection();
+
+        $list = $this->getListOfKeyValuePairs();
+        $collection->addFrom($list);
+
+        $this->assertSame($list, $collection->jsonSerialize());
+    }
+
+    /**
+     * @test
+     * @covers ::toJson
+     */
+    public function exportToJson(){
+        $collection = $this->getDummyCollection();
+
+        $list = $this->getListOfKeyValuePairs();
+        $collection->addFrom($list);
+
+        $expected = json_encode($list);
+
+        $this->assertSame($expected, $collection->toJson());
+    }
+
+    /**
+     * @test
+     * @covers ::__toString
+     */
+    public function exportToString(){
+        $collection = $this->getDummyCollection();
+
+        $list = $this->getListOfKeyValuePairs();
+        $collection->addFrom($list);
+
+        $this->assertInternalType('string', $collection->__toString());
+    }
+}
+
+class DummyPartialCollection {
+
+    use PartialCollectionTrait;
+
+    public function addFrom(array $list){
+        $this->setInternalCollection($this->getInternalCollection()->merge($list));
+    }
+
+    public function put($key, $value){
+        $this->getInternalCollection()->put($key, $value);
+    }
+
+}
