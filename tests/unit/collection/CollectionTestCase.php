50fb19d13b7baeb2cb5510c36941e63f582c07e6
Alin Eugen Deac <aedart@gmail.com>
2015-05-24 10:59:49 +0200

    Added a get list helper method

--- tests/unit/collection/CollectionTestCase.php
+++ tests/unit/collection/CollectionTestCase.php
@@ -37,6 +37,20 @@ abstract class CollectionTestCase extends \Codeception\TestCase\Test
      * Helpers
      ********************************************************************************/
 
+    /**
+     * Returns a list of key => value pairs, where the keys are
+     * unique
+     *
+     * @return array
+     */
+    protected function getListOfKeyValuePairs(){
+        return [
+            $this->faker->unique()->word => $this->faker->boolean(),
+            $this->faker->unique()->word => $this->faker->sentence(),
+            $this->faker->unique()->word => $this->faker->randomNumber(),
+        ];
+    }
+
     /*********************************************************************************
      * Actual tests
      ********************************************************************************/
