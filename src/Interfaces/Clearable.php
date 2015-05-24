7b7a88aba157a1b8e666ad3cb3a394e66edf57d5
Alin Eugen Deac <aedart@gmail.com>
2015-05-24 10:01:05 +0200

    Clearable interface completed

---
+++ src/Interfaces/Clearable.php
@@ -0,0 +1,21 @@
+<?php  namespace Aedart\Util\Interfaces; 
+
+/**
+ * Interface Clearable
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Util\Interfaces
+ */
+interface Clearable {
+
+    /**
+     * Clear this component, e.g. collection, list, set...etc.
+     *
+     * Method removes all internal values, which this component
+     * holds.
+     *
+     * @return void
+     */
+    public function clear();
+
+}
