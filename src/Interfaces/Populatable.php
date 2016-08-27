b1c8a1cd5d0bd958cf5fed511ccf3f231072a398
Alin Eugen Deac <aedart@gmail.com>
2016-08-28 00:15:38 +0200

    Change populate method, allow empty array

--- src/Interfaces/Populatable.php
+++ src/Interfaces/Populatable.php
@@ -3,9 +3,13 @@
 /**
  * Interface Populatable
  *
+ * <br />
+ *
  * Components implementing this interface promise, that the given component can
  * be populated with some kind of data and thereby set its internal properties or
  * states to the given value(s)
+ *
+ * <br />
  *
  * What kind of properties can be populated / set, is completely implementation
  * dependent.
@@ -17,6 +21,15 @@ interface Populatable {
 
     /**
      * Populate this component via an array
+     *
+     * <br />
+     *
+     * If an empty array is provided, nothing is populated.
+     *
+     * <br />
+     *
+     * If a value or property is not given via $data, then it
+     * is NOT modified / changed.
      *
      * @param array $data Key-value pair, where the key corresponds to a
      * property name and the value to be set, e.g. <p>
@@ -32,6 +45,6 @@ interface Populatable {
      *
      * @throws \Exception In case that one or more of the given array entries are invalid
      */
-    public function populate(array $data);
+    public function populate(array $data = []);
 
 }
