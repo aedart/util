a8ee81a27c4310194208b7b12ad0fbef23799161
Alin Eugen Deac <aedart@gmail.com>
2017-09-03 15:31:56 +0200

    Refactor internalCollection trait
    Turned into a normal getter setter trait, but with protected methods

--- src/Traits/Collections/InternalCollectionTrait.php
+++ src/Traits/Collections/InternalCollectionTrait.php
@@ -15,51 +15,63 @@ use Illuminate\Support\Collection;
  */
 trait InternalCollectionTrait
 {
-
     /**
-     * This collection's internal collection.
-     * It is being used to store the actual values
+     * Internal Collection instance
      *
-     * @var \Illuminate\Support\Collection|null
+     * @var Collection|null
      */
     protected $internalCollection = null;
 
     /**
-     * Set the internal collection, which contain the actual
-     * values of this collection
+     * Set internal collection
      *
-     * @param \Illuminate\Support\Collection $internalCollection
+     * @param Collection|null $collection Internal Collection instance
+     *
+     * @return self
      */
-    protected function setInternalCollection(Collection $internalCollection)
+    protected function setInternalCollection(?Collection $collection)
     {
-        $this->internalCollection = $internalCollection;
+        $this->internalCollection = $collection;
+
+        return $this;
     }
 
     /**
-     * Get the internal collection, which contain the actual
-     * values of this collection
+     * Get internal collection
      *
      * If no internal collection has been set, this method will
-     * set and return a default empty collection
+     * set and return a default internal collection, if any such
+     * value is available
      *
-     * @return \Illuminate\Support\Collection
+     * @see getDefaultInternalCollection()
+     *
+     * @return Collection|null internal collection or null if none internal collection has been set
      */
-    protected function getInternalCollection()
+    protected function getInternalCollection(): ?Collection
     {
-        if (is_null($this->internalCollection)) {
+        if (!$this->hasInternalCollection()) {
             $this->setInternalCollection($this->getDefaultInternalCollection());
         }
         return $this->internalCollection;
     }
 
     /**
-     * Returns a default empty collection
+     * Check if internal collection has been set
      *
-     * @return Collection A default empty collection
+     * @return bool True if internal collection has been set, false if not
      */
-    protected function getDefaultInternalCollection()
+    protected function hasInternalCollection(): bool
+    {
+        return isset($this->internalCollection);
+    }
+
+    /**
+     * Get a default internal collection value, if any is available
+     *
+     * @return Collection|null A default internal collection value or Null if no default value is available
+     */
+    protected function getDefaultInternalCollection(): ?Collection
     {
         return new Collection();
     }
-
 }
